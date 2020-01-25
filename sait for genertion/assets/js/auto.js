0f7cdb56f062d6b5274080dc9f60f4ce85836df8
Dmitry <n.diman1998@gmail.com>
2020-01-25 16:24:42 +0300

    generation_site

---
+++ sait for genertion/assets/js/auto.js
@@ -0,0 +1,10 @@
+function setheight(){
+  var clientHeight = document.body.clientHeight;
+  $('.content').height(clientHeight);
+}
+$(document).ready(function() {
+   function setheight();
+});
+$(window).resize(function(){
+   function setheight();
+});
---
+++ sait for genertion/assets/js/togglecolor.js
@@ -0,0 +1,27 @@
+$('.con').click(function () {
+      $(".content").css("background-color","#bdd974");
+})
+$('.ctw').click(function () {
+      $(".content").css("background-color","#feb45a");
+})
+$('.ctr').click(function () {
+      $(".content").css("background-color","#fcd259");
+})
+$('.cfo').click(function () {
+      $(".content").css("background-color","#232029");
+})
+$('.cfi').click(function () {
+      $(".content").css("background-color","#435673");
+})
+$('.csi').click(function () {
+      $(".content").css("background-color","#98c7de");
+})
+$('.cse').click(function () {
+      $(".content").css("background-color","#83577c");
+})
+$('.cec').click(function () {
+      $(".content").css("background-color","#101724");
+})
+$('.cni').click(function () {
+      $(".content").css("background-color","#ac8e4b");
+})
